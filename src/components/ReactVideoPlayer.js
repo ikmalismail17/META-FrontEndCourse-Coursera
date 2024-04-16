11871c0fa07bed674bebd5834f3a0b5d917651aa
ikmalismail17 <ikmal5a2013@gmail.com>
2024-04-16 09:37:38 +0800

    add audio

--- src/components/ReactVideoPlayer.js
+++ src/components/ReactVideoPlayer.js
@@ -1,9 +1,25 @@
 // import ReactPlayer from "react-player"; // use entire package functionality
 import ReactPlayerYT from "react-player/youtube"; // use only youtube functionality
+import ReactSong from "./ReactSong";
+import PropChildDiv from "./PropChildDiv";
+import video from '../assets/videos/videoquran.mp4';
 
 const ReactVideoPlayer = () => {
+
     return (
-        <ReactPlayerYT url='https://www.youtube.com/watch?v=dQw4w9WgXcQ' playing={true} volume={0.5}/>
+        <>
+        <h1>Video</h1>
+        <PropChildDiv>
+            <video height={300} controls autoPlay>
+                <source src={video} type="video/mp4" />
+            </video>
+            <ReactPlayerYT url='https://www.youtube.com/watch?v=dQw4w9WgXcQ' volume={0.5}/>
+        </PropChildDiv>
+        <h1>Audio</h1>
+        <PropChildDiv>
+            <ReactSong />
+        </PropChildDiv>
+        </>
     );
 }
 
