d170d9deb9e14ba66a786d8a6431db0c6a3a6afd
ikmalismail17 <ikmal5a2013@gmail.com>
2024-04-21 10:23:42 +0800

    Custom hook using useEffect

--- src/components/TryThemeContext.js
+++ src/components/TryThemeContext.js
@@ -1,5 +1,8 @@
 import { useTheme } from "../hooks/ThemeContext";
+import PropChildDiv from "./PropChildDiv";
 import Switch from "./Switch";
+import { useState } from "react";
+import usePrevious from "../hooks/CustomHooks";
 
 const Title = ({ children }) => {
     const { theme } = useTheme();
@@ -60,16 +63,48 @@ const Page = () => {
 
 const TryThemeContext = () => {
     const { theme } = useTheme();
+    const [day, setDay] = useState("Monday");
+    const prevDay = usePrevious(day);
+    const getNextDay = () => {
+        if (day === "Monday") {
+        setDay("Tuesday")
+        } else if (day === "Tuesday") {
+        setDay("Wednesday")
+        } else if (day === "Wednesday") {
+        setDay("Thursday")
+        } else if (day === "Thursday") {
+        setDay("Friday")
+        } else if (day === "Friday") {
+        setDay("Monday")
+        }
+    }
+
     return (
-        <div
-        className="App"
-        style={{
-            backgroundColor: theme === "light" ? "white" : "#282c34",
-        }}
-        >
-        <Header />
-        <Page />
-        </div>
+        <>
+            <div
+            className="App"
+            style={{
+                backgroundColor: theme === "light" ? "white" : "#282c34",
+            }}
+            >
+            <Header />
+            <Page />
+            </div>
+            <PropChildDiv>
+            <p>Custom Hook</p>
+            <h1>
+                Today is: {day}<br />
+                {
+                prevDay && (
+                    <span>Previous work day was: {prevDay}</span>
+                )
+                }
+            </h1>
+            <button onClick={getNextDay}>
+                Get next day
+            </button>
+            </PropChildDiv>
+        </>
     );
 };
 
